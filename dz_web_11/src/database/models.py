c26df161d03285f5ec74d08052d4e1e309c189ef
Andriy <127453425+Andriy22226@users.noreply.github.com>
2023-10-08 16:18:29 +0300

    Add files via upload

---
+++ dz_web_11/src/database/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.sql.sqltypes import Date
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Contact(Base):
+    __tablename__ = "contacts"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    last_name = Column(String, nullable=True)
+    phone = Column(String, nullable=False)
+    email = Column(String, nullable=True)
+    birthday = Column('b_day', Date, nullable=True)
+    description = Column(Text, nullable=True)
+ 
+
+
